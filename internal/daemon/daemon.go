29560c0daa8dd9f961512e1c8507121aaee95df5
Gabriel Mougard <gabriel.mougard@canonical.com>
2024-02-23 10:54:04 +0100

    microcluster/internal/db: Allow the `db.Open` to take an `*Extensions` parameter
    In order to compare this node's extensions to the other nodes, we need to pass the daemon's extension
    to the `db.Open` function so that the extensions check happens at the same time as the DB schema check.
    
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -398,7 +398,7 @@ func (d *Daemon) StartAPI(bootstrap bool, initConfig map[string]string, newConfi
 			Role:        cluster.Pending,
 		}
 
-		err = d.db.Bootstrap(d.project, d.address, clusterMember)
+		err = d.db.Bootstrap(d.Extensions, d.project, d.address, clusterMember)
 		if err != nil {
 			return err
 		}
@@ -418,12 +418,12 @@ func (d *Daemon) StartAPI(bootstrap bool, initConfig map[string]string, newConfi
 	}
 
 	if len(joinAddresses) != 0 {
-		err = d.db.Join(d.project, d.address, joinAddresses...)
+		err = d.db.Join(d.Extensions, d.project, d.address, joinAddresses...)
 		if err != nil {
 			return fmt.Errorf("Failed to join cluster: %w", err)
 		}
 	} else {
-		err = d.db.StartWithCluster(d.project, d.address, d.trustStore.Remotes().Addresses())
+		err = d.db.StartWithCluster(d.Extensions, d.project, d.address, d.trustStore.Remotes().Addresses())
 		if err != nil {
 			return fmt.Errorf("Failed to re-establish cluster connection: %w", err)
 		}
